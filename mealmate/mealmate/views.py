5a402d3c821e172cf11acafdb7a4926d8eaadc73
Jun Park <90596660+CA-JunPark@users.noreply.github.com>
2024-03-23 15:07:01 -0700

    delete posts when over

--- mealmate/mealmate/views.py
+++ mealmate/mealmate/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from post.models import Post
 from account.models import Account
+import datetime
 
 class Home(APIView):
     def get(self, request):
@@ -13,17 +14,22 @@ class Home(APIView):
         posts_objects = Post.objects.all()
         
         posts = []
+        
         for post in posts_objects:
-            owner_object = Account.objects.get(email=post.owner)
-            posts.append(dict(id = post.id, 
-                              owner=post.owner, 
-                              owner_name=owner_object.username,
-                              photo=owner_object.photo, 
-                              where=post.where,
-                              Note=post.Note,
-                              current_user_number=post.current_user_number,
-                              current_users=post.current_users,
-                              max_user_num=post.max_user_num,
-                              when=post.when))
-            
+            # if current time is before post.when
+            if post.when > datetime.datetime.now().time():
+                owner_object = Account.objects.get(email=post.owner)
+                posts.append(dict(id = post.id, 
+                                owner=post.owner, 
+                                owner_name=owner_object.username,
+                                photo=owner_object.photo, 
+                                where=post.where,
+                                Note=post.Note,
+                                current_user_number=post.current_user_number,
+                                current_users=post.current_users,
+                                max_user_num=post.max_user_num,
+                                when=post.when))
+            else: # delete posts that are over
+                post.delete()
+                
         return render(request, 'mealmate/home.html', context={'posts': posts, 'user': user_objects})
