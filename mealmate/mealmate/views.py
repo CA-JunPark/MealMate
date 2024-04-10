1a09ba1fca54b392bda4bc2d21ca10edd0c28fa7
Jun Park <90596660+CA-JunPark@users.noreply.github.com>
2024-04-09 17:00:16 -0700

    save

--- mealmate/mealmate/views.py
+++ mealmate/mealmate/views.py
@@ -17,35 +17,44 @@ class Home(APIView):
         print(sortVal, searchBy, searchVal)
         
         user_objects = Account.objects.get(email=user.email)
-        if searchVal is None: 
+        if searchVal is None or searchVal == "": 
             posts_objects = Post.objects.all()
         else:
-            if searchBy == "owner":
+            if searchBy == "ownerName":
                 posts_objects = Post.objects.get(ownerName=searchVal)
-            # TODO so on...
+            elif searchBy == "where":
+                posts_objects = Post.objects.get(where=searchVal)
+            elif searchBy == "when":
+                posts_objects = Post.objects.get(when=searchVal)
+            elif searchBy == "current_user_number":
+                posts_objects = Post.objects.get(current_user_number=searchVal)
+            elif searchBy == "max_user_num":
+                posts_objects = Post.objects.get(max_user_num=searchVal)
         
         posts = []
         
-        for post in posts_objects:
-            # if current time is before post.when
-            if post.when > datetime.datetime.now().time():
-                owner_object = Account.objects.get(email=post.owner)
-                posts.append(dict(id = post.id, 
-                                owner=post.owner, 
-                                owner_name=owner_object.username,
-                                photo=owner_object.photo, 
-                                where=post.where,
-                                Note=post.Note,
-                                current_user_number=post.current_user_number,
-                                current_users=post.current_users,
-                                max_user_num=post.max_user_num,
-                                when=post.when))
-            else: # delete posts that are over
-                post.delete()
+        if posts_objects.count() > 0:
+            for post in posts_objects:
+                # if current time is before post.when
+                if post.when > datetime.datetime.now().time():
+                    owner_object = Account.objects.get(email=post.owner)
+                    posts.append(dict(id = post.id, 
+                                    owner=post.owner, 
+                                    owner_name=owner_object.username,
+                                    photo=owner_object.photo, 
+                                    where=post.where,
+                                    Note=post.Note,
+                                    current_user_number=post.current_user_number,
+                                    current_users=post.current_users,
+                                    max_user_num=post.max_user_num,
+                                    when=post.when))
+                else: # delete posts that are over
+                    post.delete()
         
         # sort 
         if sortVal is not None:
-            posts = sorted(posts, key = lambda item: item[sortVal], reverse=True)
+            print(sortVal)
+            posts = sorted(posts, key = lambda item: item["where"], reverse=True)
         else:
             posts = sorted(posts, key=lambda item: item["when"], reverse=False)
         return render(request, 'mealmate/home.html', context={'posts': posts, 'user': user_objects})
